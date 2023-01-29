8be3c36dc5b58c1106a0fedce7ec3cf959a3f53f
Carolina Rodriguez Medina <110941300+carry0xn@users.noreply.github.com>
2023-01-29 18:03:35 -0300

    js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,6 @@
+//seleccionamos el div desplegable
+const DESPLEGABLE = document.getElementById("desplegable");
+//creamos una función que muestre o esconda el desplegable
 function mostrarNavMobile(){
     DESPLEGABLE.classList.toggle("mostrar_desplegable");
 }
