3b070be66daf1d77c9b6f9aff4e428b6c670cf46
Carolina Rodriguez Medina <110941300+carry0xn@users.noreply.github.com>
2023-06-21 11:34:16 -0300

    css

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,3 +4,15 @@ const DESPLEGABLE = document.getElementById("desplegable");
 function mostrarNavMobile(){
     DESPLEGABLE.classList.toggle("mostrar_desplegable");
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+    var timelineCards = document.querySelectorAll('.timeline__card');
+  
+    // Agregamos la clase 'animation' a cada elemento de la línea de tiempo
+    timelineCards.forEach(function(card, index) {
+      setTimeout(function() {
+        card.classList.add('animation');
+      }, index * 300); // Ajusta el retraso (300ms en este caso) para lograr el efecto deseado
+    });
+  });
+  
