d1aa41d43cef51dbd79649fea7e7f442a969a7a3
Carolina Rodriguez Medina <110941300+carry0xn@users.noreply.github.com>
2023-12-12 19:47:01 -0300

    version en ingles

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,10 +1,3 @@
-//seleccionamos el div desplegable
-const DESPLEGABLE = document.getElementById("desplegable");
-//creamos una función que muestre o esconda el desplegable
-function mostrarNavMobile(){
-    DESPLEGABLE.classList.toggle("mostrar_desplegable");
-}
-
 document.addEventListener('DOMContentLoaded', function() {
     var timelineCards = document.querySelectorAll('.timeline__card');
   
@@ -15,4 +8,136 @@ document.addEventListener('DOMContentLoaded', function() {
       }, index * 300); // Ajusta el retraso (300ms en este caso) para lograr el efecto deseado
     });
   });
-  
+
+  Vue.component('porfolio-app', {
+    template: `
+        <div class="proyectos">
+          <div class="box">
+            <div class="imgBx">
+              <img src="./assets/img/comments-python.jpg">
+            </div>
+            <div class="content">
+                <div>
+                    <h2>Proyectos Python</h2>
+                    <a href="https://github.com/carry0xn/Python" target="_blank"><i class="fa fa-fw fa-github"></i> Repositorio Python </a>
+                </div>
+            </div>
+          </div>
+          <div class="box">
+            <div class="imgBx">
+              <img src="./assets/img/javascript-code.jpg">
+            </div>
+            <div class="content">
+                <div>
+                    <h2>Proyectos Web</h2>
+                    <a href="https://github.com/carry0xn/web-development" target="_blank"><i class="fa fa-fw fa-github"></i> Repositorio Web </a>
+                </div>
+            </div>
+          </div>
+
+          <div class="box">
+            <div class="imgBx">
+                <img src="./assets/img/desarrollo-web-completo-con-html5-css3-js-php-y-mysql.png" target="_blank">
+            </div>
+            <div class="content">
+                <div>
+                    <h2>Proyecto Web</h2>
+                    <p>Fue realizado en grupo</p>
+                    <a href="https://github.com/JaviMun/CodoaCodo" target="_blank"><i class="fa fa-fw fa-github"></i> Repositorio Web </a>
+                </div>
+            </div>
+          </div>
+          <div class="box">
+            <div class="imgBx">
+                <img src="./assets/img/para-que-sirve-lenguaje-sql.jpg">
+            </div>
+            <div class="content">
+                <div>
+                    <h2>Proyectos SQL</h2>
+                    <a href="https://github.com/carry0xn/SQL" target="_blank"><i class="fa fa-fw fa-github"></i> Repositorio SQL </a>
+                </div>
+            </div>
+          </div>
+          <div class="box">
+            <div class="imgBx">
+                <img src="./assets/img/dataScience.png">
+            </div>
+            <div class="content">
+                <div>
+                    <h2>Proyectos de Data Science</h2>
+                    <a href="https://github.com/carry0xn/DataScience" target="_blank"><i class="fa fa-fw fa-github"></i> Repositorio Data Science </a>
+                </div>
+            </div>
+          </div>
+        </div>
+    `
+});
+
+Vue.component('porfolio-app-ingles', {
+  template: `
+      <div class="proyectos">
+        <div class="box">
+          <div class="imgBx">
+            <img src="./assets/img/comments-python.jpg">
+          </div>
+          <div class="content">
+              <div>
+                  <h2>Python Proyects</h2>
+                  <a href="https://github.com/carry0xn/Python" target="_blank"><i class="fa fa-fw fa-github"></i> Repository </a>
+              </div>
+          </div>
+        </div>
+        <div class="box">
+          <div class="imgBx">
+            <img src="./assets/img/javascript-code.jpg">
+          </div>
+          <div class="content">
+              <div>
+                  <h2>Web Proyects</h2>
+                  <a href="https://github.com/carry0xn/web-development" target="_blank"><i class="fa fa-fw fa-github"></i> Repository </a>
+              </div>
+          </div>
+        </div>
+
+        <div class="box">
+          <div class="imgBx">
+              <img src="./assets/img/desarrollo-web-completo-con-html5-css3-js-php-y-mysql.png" target="_blank">
+          </div>
+          <div class="content">
+              <div>
+                  <h2>Web Project</h2>
+                  <p>Executed in collaboration with a team</p>
+                  <a href="https://github.com/JaviMun/CodoaCodo" target="_blank"><i class="fa fa-fw fa-github"></i> Repository </a>
+              </div>
+          </div>
+        </div>
+        <div class="box">
+          <div class="imgBx">
+              <img src="./assets/img/para-que-sirve-lenguaje-sql.jpg">
+          </div>
+          <div class="content">
+              <div>
+                  <h2>SQL Proyects </h2>
+                  <a href="https://github.com/carry0xn/SQL" target="_blank"><i class="fa fa-fw fa-github"></i> Repository </a>
+              </div>
+          </div>
+        </div>
+        <div class="box">
+          <div class="imgBx">
+              <img src="./assets/img/dataScience.png">
+          </div>
+          <div class="content">
+              <div>
+                  <h2>Data Science Proyects </h2>
+                  <a href="https://github.com/carry0xn/DataScience" target="_blank"><i class="fa fa-fw fa-github"></i> Repository </a>
+              </div>
+          </div>
+        </div>
+      </div>
+  `
+});
+
+new Vue({
+    el: '#app',
+});
+
