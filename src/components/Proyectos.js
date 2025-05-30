e953741778ac2da00143c3e0be221e05c3f1631a
carry0xn <rodriguezc.medina@gmail.com>
2025-05-29 21:51:28 -0300

    less repository

--- src/components/Proyectos.js
+++ src/components/Proyectos.js
@@ -25,31 +25,6 @@ const Proyectos = () => {
   return (
     <section id="proyectos">
       <div className="proyectos">
-        {/* Proyecto Python */}
-        <div className="flip-card">
-          <div className="flip-card-inner">
-            <div className="flip-card-front">
-              <div className="imgBx">
-                <img src={python} alt="python-img" />
-              </div>
-              <div className="content">
-                <div>
-                  <h2>{t('proyectos.python.titulo')}</h2>
-                </div>
-              </div>
-            </div>
-            <div className="flip-card-back">
-              <div className="content">
-                <div>
-                  <h2>{t('proyectos.python.titulo')}</h2>
-                  <a href="https://github.com/carry0xn/Python" target="_blank" rel="noopener noreferrer">
-                    <FontAwesomeIcon icon={faGithub} /> {t('proyectos.python.repositorio')}
-                  </a>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
 
         {/* Proyecto Calculadora */}
         <div className="flip-card">
@@ -149,32 +124,6 @@ const Proyectos = () => {
                   <p>{t('proyectos.proyectoWeb.descripcion')}</p>
                   <a href="https://github.com/JaviMun/CodoaCodo" target="_blank" rel="noopener noreferrer">
                     <FontAwesomeIcon icon={faGithub} /> {t('proyectos.proyectoWeb.repositorio')}
-                  </a>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        {/* Proyecto SQL */}
-        <div className="flip-card">
-          <div className="flip-card-inner">
-            <div className="flip-card-front">
-              <div className="imgBx">
-                <img src={sql} alt="sql-img" />
-              </div>
-              <div className="content">
-                <div>
-                  <h2>{t('proyectos.sql.titulo')}</h2>
-                </div>
-              </div>
-            </div>
-            <div className="flip-card-back">
-              <div className="content">
-                <div>
-                  <h2>{t('proyectos.sql.titulo')}</h2>
-                  <a href="https://github.com/carry0xn/SQL" target="_blank" rel="noopener noreferrer">
-                    <FontAwesomeIcon icon={faGithub} /> {t('proyectos.sql.repositorio')}
                   </a>
                 </div>
               </div>
